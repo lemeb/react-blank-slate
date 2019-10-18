6f49329a413c614bd823cb908d9baff241653b07
Léopold Mebazaa <lemeb@users.noreply.github.com>
2019-10-18 12:18:54 -0400

    More in-file documentation, style

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,22 +6,23 @@ import { Editor } from "slate-react";
 import BridgeManager from "../lib/BridgeManager";
 import { initialValue, initialNote } from "../lib/utils";
 
-// const log = (...arg) => { if (lEnab) { console.log(...arg) } }, lEnab = true
+const log = (...arg) => { if (lEnab) { console.log(...arg) } }, lEnab = true
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
-
     console.clear();
+    // Get defined defaults for Slate Value and SN Note
     this.state = { slateValue: initialValue };
     this.lastSNNote = initialNote;
-
     // Pass to the SN API our callback function (see below)
     BridgeManager.get().addUpdateObserver(this.handleUpdateFromSN);
   }
 
-  // Resets the editor's state when a change comes from SN
-  // A totally in place callback function.
+  /*
+   * Resets the editor's state when a change comes from SN
+   * A totally in place callback function.
+   */
   handleUpdateFromSN = () => {
     var noteFromSN = BridgeManager.get().getNote();
     if (this.lastSNNote == initialNote) {
@@ -30,29 +31,41 @@ export default class Home extends React.Component {
     this.lastSNNote = noteFromSN;
   };
 
-  // Resets the editor's state when a change is made in Slate.
-  // @param { Value } value
+  /*
+   * Resets the editor's state when a change is made in Slate.
+   * @param     { Map }         change
+   * @param     { Value }       change.value
+   * @param     { [ Operation ] }   change.operation
+   */
   handleEditFromSlate = change => {
     let str = this.slateToSN(change.value);
+    log(change.operations.map(op => op.type).toArray())
     if (str) {
       BridgeManager.get().saveNote(str);
       this.setState({ slateValue: change.value });
     }
   };
 
-  // Converts a SN note to a Slate Value
+  /*
+   * Converts a SN note to a Slate Value
+   * @param     { Object }      note
+   * @return    { Value }       value with note in it
+   */
   snToSlate = note => {
-    if (!note) {
+    if (note) {
+      var newVal = Plain.deserialize(note.content.text);
+      return newVal.set("selection", this.state.slateValue.selection);
+    } else {
       return initialValue;
     }
-    var newValue = Plain.deserialize(note.content.text);
-    newValue = newValue.set("selection", this.state.slateValue.selection);
-    return newValue;
   };
 
-  // Converts a Slate Value object to a string that
-  // can be passed to SN. As above, will try to implement
-  // that functionality myself.
+  /*
+   * Converts a Slate Value to a SN note
+   * As above, will try to implement that myself.
+   * @param     { Value }       value
+   * @return    { String }      str the editor's contents
+   */
   slateToSN = value => {
     return Plain.serialize(value);
   };
--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,44 +1,46 @@
-import { Value } from 'slate'
-import initialValueAsJson from './value.json'
+import { Value } from "slate";
+import initialValueAsJson from "./value.json";
 
 export const log = (...arg) => {
   if (lEnab) {
-    console.log(...arg)
+    console.log(...arg);
   }
-}
-var lEnab = true
+};
+var lEnab = true;
 
 export const timestampString = dateobj => {
   if (!dateobj) {
     // gets dateobj of now if no arg passed
-    dateobj = new Date()
+    dateobj = new Date();
   }
   let ddmmyy = [
     // returns a 'dd/mm/yy' string
     fillWithZeros(dateobj.getDate(), 2),
     fillWithZeros(dateobj.getMonth() + 1, 2),
     fillWithZeros(dateobj.getFullYear() - 2000, 2)
-  ].join('/')
+  ].join("/");
   let hhmmssll = [
     // -> 'hh:mm:ss:ll' with ll millisec
     fillWithZeros(dateobj.getHours(), 2),
     fillWithZeros(dateobj.getMinutes(), 2),
     fillWithZeros(dateobj.getSeconds(), 2),
     fillWithZeros(dateobj.getMilliseconds(), 3)
-  ].join(':')
-  return ['(dd/mm/yy)', ddmmyy, hhmmssll].join(' ')
-}
+  ].join(":");
+  return ["(dd/mm/yy)", ddmmyy, hhmmssll].join(" ");
+};
 
 // if millisecond string is '6', returns '006'
 const fillWithZeros = (val, numbers) => {
-  var v = String(val)
-  var difflen = numbers - v.length
-  return '0'.repeat(difflen) + v
-}
+  var v = String(val);
+  var difflen = numbers - v.length;
+  return "0".repeat(difflen) + v;
+};
 
+/* eslint-disable camelcase*/
 // Constants that represent default values for
 // the Value Object of SlateJS and a SN Note
-export const initialValue = Value.fromJSON(initialValueAsJson)
-export const initialNote = { content: { text: '' }, updated_at: new Date() }
+export const initialValue = Value.fromJSON(initialValueAsJson);
+export const initialNote = { content: { text: "" }, updated_at: new Date() };
+/* eslint-enable camelcase*/
 
-export const e = { note: '🗒', write: '✍' }
+export const e = { note: "🗒", write: "✍" };
