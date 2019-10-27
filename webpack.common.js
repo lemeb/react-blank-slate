310ce7382ec0e19f3e7b84caa7d5dbf849a7c4c0
Léopold Mebazaa <lemeb@users.noreply.github.com>
2019-10-27 17:01:39 -0400

    Removed yarn, switched to vim

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
